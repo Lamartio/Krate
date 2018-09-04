143b8c11fa1148fba540eeb87e4630348355a77c
Danny Lamarti <lamarti.danny@gmail.com>
2018-09-04 10:03:26 +0200

    Applies a schedulerkrate on the example

--- example/src/main/java/io/lamart/krate/example/ExampleApplication.kt
+++ example/src/main/java/io/lamart/krate/example/ExampleApplication.kt
@@ -4,19 +4,22 @@ import android.app.Application
 import android.database.sqlite.SQLiteDatabase
 import com.facebook.soloader.SoLoader
 import io.lamart.krate.Krate
+import io.lamart.krate.SchedulerKrate
 import io.lamart.krate.database.DatabaseKrate
-import java.io.File
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 
 class ExampleApplication : Application() {
 
     val krate: Krate by lazy {
-        cacheDir.apply { mkdirs() }
-                .let { File(it, "database") }
+        "example_database"
+                .let(::getDatabasePath)
                 .apply { createNewFile() }
                 .let { SQLiteDatabase.openOrCreateDatabase(it, null) }
                 .let { DatabaseKrate(database = it, interceptor = ConcealInterceptor(this)) }
                 .createTableIfNotExists()
+                .let { SchedulerKrate(krate = it, ioScheduler = Schedulers.io(), resultScheduler = AndroidSchedulers.mainThread()) }
     }
 
     override fun onCreate() {
