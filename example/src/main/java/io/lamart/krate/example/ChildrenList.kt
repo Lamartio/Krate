96d547b4c726d3a799a6bb22f76a0dd930355942
Danny Lamarti <lamarti.danny@gmail.com>
2018-09-05 11:18:38 +0200

    Small update for the example

--- example/src/main/java/io/lamart/krate/example/ChildrenList.kt
+++ example/src/main/java/io/lamart/krate/example/ChildrenList.kt
@@ -5,31 +5,31 @@ import android.view.ViewGroup
 
 class ChildrenList(val parent: ViewGroup) : List<View> {
 
-    private val ViewGroup.children: List<View>
-        get() = 0.until(childCount).map(::getChildAt)
+    private val children: List<View>
+        get() = 0.until(parent.childCount).map(parent::getChildAt)
 
     override val size: Int
         get() = parent.childCount
 
-    override fun contains(element: View): Boolean = parent.children.contains(element)
+    override fun contains(element: View): Boolean = children.contains(element)
 
     override fun containsAll(elements: Collection<View>): Boolean =
-            parent.children.containsAll(elements)
+            children.containsAll(elements)
 
-    override fun get(index: Int): View = parent.children.get(index)
+    override fun get(index: Int): View = children.get(index)
 
-    override fun indexOf(element: View): Int = parent.children.indexOf(parent)
+    override fun indexOf(element: View): Int = children.indexOf(parent)
 
-    override fun isEmpty(): Boolean = parent.childCount == 0
+    override fun isEmpty(): Boolean = size == 0
 
-    override fun iterator(): Iterator<View> = parent.children.iterator()
+    override fun iterator(): Iterator<View> = children.iterator()
 
-    override fun lastIndexOf(element: View): Int = parent.children.lastIndexOf(element)
+    override fun lastIndexOf(element: View): Int = children.lastIndexOf(element)
 
-    override fun listIterator(): ListIterator<View> = parent.children.listIterator()
+    override fun listIterator(): ListIterator<View> = children.listIterator()
 
-    override fun listIterator(index: Int): ListIterator<View> = parent.children.listIterator(index)
+    override fun listIterator(index: Int): ListIterator<View> = children.listIterator(index)
 
     override fun subList(fromIndex: Int, toIndex: Int): List<View> =
-            parent.children.subList(fromIndex, toIndex)
+            children.subList(fromIndex, toIndex)
 }
