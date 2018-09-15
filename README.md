a8e8331669aadffaa06bf1015d8a48dc12fede29
Danny Lamarti <lamarti.danny@gmail.com>
2018-09-15 17:12:16 +0200

    Updates the readme with examples.

--- README.md
+++ README.md
@@ -2,4 +2,54 @@
 [ ![Download](https://api.bintray.com/packages/lamartio/maven/krate/images/download.svg) ](https://bintray.com/lamartio/maven/krate/_latestVersion) 
 [ ![Coverage](https://img.shields.io/badge/Coverage-94%25-brightgreen.svg) ](https://bintray.com/lamartio/maven/krate/_latestVersion) 
 
-Simple storage and network layer for Android.
+Simple storage and network layer for Android. It is a key value store that accept any object as its value. Additionally it provides utility for encryption and custom serializers.
+
+A Krate is an interface that has implementations for SQLiteDatabase, Files and in-memory. It supplies its operations in a reactive fashion:
+
+``` kotlin
+fun crud(krate: Krate) {
+    val getter: Maybe<User> = krate.get("k")
+
+    getter.subscribe { user -> }
+
+    krate.put("k", User()) // lets store a new user
+            .andThen { krate.remove("k") } // and then remove it 
+            .subscribe()
+}
+```
+
+Apps often rely on a webservice to provide objects. Krate is great for this data centric approach, since it persists the result of a network call before passing it through.
+
+```kotlin
+// first gets it from persistence
+// next it does the network call and persist the result
+// emits 0, 1 or 2 results.
+
+fun network(krate: Krate, getUserFromApi: () -> Single<User>): Flowable<User> = 
+        krate.getAndFetch("k", getUserFromApi)
+```
+
+Based on your needs you can decide you persistence method. Image are often not welcome to a database, so Krate can manage a directory for you:
+
+```kotlin
+fun krates(context: Context, picture: ByteArray) {
+    val dirKrate = DirectoryKrate(context.cacheDir)
+    val sqlKrate = DatabaseKrate(context.openOrCreateDatabase("test", Context.MODE_PRIVATE, null))
+    
+    dirKrate.put("giantPicture", picture).subscribe()
+    sqlKrate.put("smallObject", Any())
+}
+```
+
+You may have the need of encryption or you may want use an alternative serializer. Krate's implementations provide easy access for this use cases:
+```kotlin
+fun customKrate(context: Context) {
+    DirectoryKrate(
+            directory = context.cacheDir,
+            serializer = CustomSerializer(), // swap the default Java serialization 
+            interceptor = CustomInterceptor() // manipulate the bytes
+    )
+}
+```
+
+Copyright
---
+++ library/src/test/java/io/lamart/krate/example/example.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.lamart.krate.example
+
+import android.content.Context
+import io.lamart.krate.Interceptor
+import io.lamart.krate.Krate
+import io.lamart.krate.Serializer
+import io.lamart.krate.database.DatabaseKrate
+import io.lamart.krate.directory.DirectoryKrate
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Single
+
+
+class User(val name: String = "Danny")
+
+fun crud(krate: Krate) {
+    val getter: Maybe<User> = krate.get("k")
+
+    getter.subscribe { user -> }
+
+    krate.put("k", User()) // lets store a new user
+            .andThen { krate.remove("k") } // and then remove it
+            .subscribe()
+}
+
+// first gets it from persistence
+// next it does the network call and persist the result
+// emits 0, 1 or 2 results.
+
+fun network(krate: Krate, getUserFromApi: () -> Single<User>): Flowable<User> =
+        krate.getAndFetch("k", getUserFromApi)
+
+fun krates(context: Context, picture: ByteArray) {
+    val dirKrate = DirectoryKrate(context.cacheDir)
+    val sqlKrate = DatabaseKrate(context.openOrCreateDatabase("test", Context.MODE_PRIVATE, null))
+
+    dirKrate.put("giantPicture", picture).subscribe()
+    sqlKrate.put("smallObject", Any())
+}
+
+
+class CustomSerializer : Serializer by Serializer.Default()
+class CustomInterceptor : Interceptor by Interceptor.Default
+
+fun customKrate(context: Context) {
+    DirectoryKrate(
+            directory = context.cacheDir,
+            serializer = CustomSerializer(), // swap the default Java serialization
+            interceptor = CustomInterceptor() // manipulate the bytes
+    )
+}
